b4b2aaccb208ef2b66cf70187f0fdc5f6af8b68d
Victor Reyes Galvez <tenshi98@gmail.com>
2021-08-17 21:38:02 -0400

    Eliminacion footer
    Se elimina el footer por ser innecesario

--- obtener imagen google.php
+++ obtener imagen google.php
@@ -62,21 +62,13 @@
 				
 			</div>
 
-			<footer class="my-5 pt-5 text-muted text-center text-small">
-				<p class="mb-1">&copy; 2017-2018 Company Name</p>
-				<ul class="list-inline">
-					<li class="list-inline-item"><a href="#">Privacy</a></li>
-					<li class="list-inline-item"><a href="#">Terms</a></li>
-					<li class="list-inline-item"><a href="#">Support</a></li>
-				</ul>
-			</footer>
+			
 		</div>
 
 		<!-- Bootstrap core JavaScript
 		================================================== -->
 		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
 		<script src="resources/js/popper.min.js"></script>
 		<script src="resources/js/bootstrap.min.js"></script>
 		<script src="resources/js/holder.min.js"></script>
